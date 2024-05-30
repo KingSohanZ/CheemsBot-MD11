227097553a5c050bfb38edbe6ea91cc674a698c9
SohanX_Mathiya <130471209+KingSohanZ@users.noreply.github.com>
2024-05-30 17:30:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,20 +18,20 @@ global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
-//new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+//
+global.botname = 'Queen Razona Bot MD V11' //ur bot name
+global.ownernumber = '94725114153' //ur owner number
+global.ownername = '🦄드림 가이 Sohan' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "🦄드림 가이 Xeon\n\n+94725114153"
+global.creator = "94725114153@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["94725114153"] // Premium User
 global.hituet = 0
 
 //bot sett
